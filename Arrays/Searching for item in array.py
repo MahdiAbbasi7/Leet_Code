aefccd81328c0c17a0b9fb53b4a644514bfec8b6
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-10-29 22:20:20 +0330

    Searching

---
+++ Arrays/Searching for item in array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkIfExist(self, arr: list[int]) -> bool:
+        # for i in range(len(arr)):
+        #     hashTable = list(arr[0, i-1])
+        # print(hashTable)
+        for i in range(len(arr)):
+            #todo: complete this line.
+            pass
+
+test = Solution()
+test.checkIfExist([10,2,5,3])
