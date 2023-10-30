443d6fe1da9ed1e61365d3043e72d8fac36501b2
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-10-30 23:28:28 +0330

    Searching (python) not (python3)

--- Arrays/Searching for item in array.py
+++ Arrays/Searching for item in array.py
@@ -1,11 +1,15 @@
 class Solution:
     def checkIfExist(self, arr: list[int]) -> bool:
-        # for i in range(len(arr)):
-        #     hashTable = list(arr[0, i-1])
-        # print(hashTable)
-        for i in range(len(arr)):
-            #todo: complete this line.
-            pass
+        count=0
+        for i in arr:
+            if(i*2 in arr and i!=0):
+                return True
+            if(i==0):
+                count+=1
+        if(count>1):
+            return True
+            
+        return False
 
 test = Solution()
 test.checkIfExist([10,2,5,3])
