5d4e8d4ff8f7b1ae7d148c5673cecd33b4451b50
Mahdi Abbasi <abbasimahdi782@gmail.com>
2024-03-15 12:19:27 +0330

    Django/AI Quera

--- Quera/cafenet.py
+++ Quera/cafenet.py
@@ -1,23 +1,6 @@
-# n, m = map(int, input().split())
-# computers = "0" * n
-# result = []
-
-# for _ in range(m):
-#     s, l = map(int, input().split())
-#     slice_ = computers[s - 1 :]
-#     requested_chairs = "0" * l
-#     idx = slice_.find(requested_chairs)
-#     if idx != -1:
-#         computers = list(computers)
-#         computers[s - 1 + idx : s - 1 + idx + l] = "1" * l
-#         computers = "".join(computers)
-#     result.append(computers)
-        
-# print('\n'.join(result))
-
-
 n, m = map(int, input().split())
 computers = "0" * n
+result = []
 
 for _ in range(m):
     s, l = map(int, input().split())
@@ -26,6 +9,12 @@ for _ in range(m):
     idx = slice_.find(requested_chairs)
     if idx != -1:
         computers = list(computers)
-        computers[s - 1 + idx : s - 1 + idx + l] = "1" * l
+        computers[s- 1 + idx : s -1 + idx + l] = "1" * l
+        '''
+            s - 1 + idx calculates the starting index for the slice (since indexing is 0-based).
+            s - 1 + idx + l calculates the ending index for the slice.
+        '''
         computers = "".join(computers)
-        print(computers)
+    result.append(computers)
+        
+print('\n'.join(result))
