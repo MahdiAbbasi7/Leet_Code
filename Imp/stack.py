cb99549eea1fdf02da91cc54a9e2f12278218e37
Mahdi Abbasi <abbasimahdi782@gmail.com>
2024-04-22 16:36:37 +0330

    stack

---
+++ Imp/stack.py
@@ -0,0 +1,30 @@
+stack = []
+
+def display():
+    print(stack)
+
+def push():
+    input_ = int(input('Enter the number for pushing:'))
+    stack.append(input_)
+    print('Successfully pushed')
+
+def pop():
+    temp=stack[-1]
+    stack.pop()
+    print(temp, "is deleted.")
+
+while(1):
+    print(" menu ")
+    print("1-push")
+    print("2-pop")
+    print("3-display")
+    print("4-exit")
+    choice=input("enter choice-")
+    if choice=="1":
+        push()
+    elif choice=="2":
+        pop()
+    elif choice=="3":
+        display()
+    elif choice=="4":
+        exit(0)
